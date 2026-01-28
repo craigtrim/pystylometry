2ccaeb77d04e72d029410c2afe4868f195d07117
Craig Trim <craigtrim@gmail.com>
2026-01-28 10:11:34 -0600

    chore: Remove unused pytest import

--- tests/test_stylistic_markers.py
+++ tests/test_stylistic_markers.py
@@ -22,8 +22,6 @@ Test coverage:
 
 import math
 
-import pytest
-
 from pystylometry.stylistic.markers import (
     ALL_MODALS,
     CONTRACTIONS,
@@ -290,8 +288,18 @@ class TestHedgeDetection:
         """Test that hedging density is correctly calculated."""
         # 50 words with 10 hedges = 20 per 100 words
         base_words = ["word"] * 40
-        hedges_list = ["maybe", "perhaps", "probably", "possibly", "seemingly",
-                       "apparently", "roughly", "generally", "usually", "typically"]
+        hedges_list = [
+            "maybe",
+            "perhaps",
+            "probably",
+            "possibly",
+            "seemingly",
+            "apparently",
+            "roughly",
+            "generally",
+            "usually",
+            "typically",
+        ]
         text = " ".join(base_words + hedges_list)
         result = compute_stylistic_markers(text)
 
@@ -688,9 +696,24 @@ class TestContractionsListCompleteness:
     def test_all_negative_contractions_present(self) -> None:
         """Test that all common negative contractions are in the list."""
         negative_contractions = [
-            "aren't", "can't", "couldn't", "didn't", "doesn't", "don't",
-            "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't",
-            "needn't", "shouldn't", "wasn't", "weren't", "won't", "wouldn't"
+            "aren't",
+            "can't",
+            "couldn't",
+            "didn't",
+            "doesn't",
+            "don't",
+            "hadn't",
+            "hasn't",
+            "haven't",
+            "isn't",
+            "mightn't",
+            "mustn't",
+            "needn't",
+            "shouldn't",
+            "wasn't",
+            "weren't",
+            "won't",
+            "wouldn't",
         ]
         for contraction in negative_contractions:
             assert contraction in CONTRACTIONS
@@ -698,13 +721,31 @@ class TestContractionsListCompleteness:
     def test_all_pronoun_contractions_present(self) -> None:
         """Test that all common pronoun contractions are in the list."""
         pronoun_contractions = [
-            "i'm", "i've", "i'll", "i'd",
-            "you're", "you've", "you'll", "you'd",
-            "he's", "he'll", "he'd",
-            "she's", "she'll", "she'd",
-            "it's", "it'll", "it'd",
-            "we're", "we've", "we'll", "we'd",
-            "they're", "they've", "they'll", "they'd"
+            "i'm",
+            "i've",
+            "i'll",
+            "i'd",
+            "you're",
+            "you've",
+            "you'll",
+            "you'd",
+            "he's",
+            "he'll",
+            "he'd",
+            "she's",
+            "she'll",
+            "she'd",
+            "it's",
+            "it'll",
+            "it'd",
+            "we're",
+            "we've",
+            "we'll",
+            "we'd",
+            "they're",
+            "they've",
+            "they'll",
+            "they'd",
         ]
         for contraction in pronoun_contractions:
             assert contraction in CONTRACTIONS
